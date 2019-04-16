868d88fdbd77d25d0d847c699cb85cafeb7ba392
Tony Bedford <tony.bedford@vonage.com>
2019-04-16 12:23:54 +0100

    Refactor to make code more readable.

--- app.py
+++ app.py
@@ -1,79 +1,13 @@
-import sys
 from flask import Flask, request, jsonify
 from pprint import pprint
-from Client.Client import Client
-from threading import Timer
+from app_funcs import update_users, proc_inbound_msg
 
-NEXMO_APP_ID = sys.argv[1]
-print("Nexmo App ID: %s" % NEXMO_APP_ID)
-
-client = Client(NEXMO_APP_ID, "private.key")
 app = Flask(__name__)
-
-users = [] # array of registered users
-
-nexmo = {}
-nexmo['number'] = sys.argv[2]
-
-# Simulation of our real-time data feed
-# Arrays should be of same length
-prices = {
-    'MSFT': ['120.24', '119.36', '118.34', '119.78', '120.23', '122.98', '140.21', '141.09', '144.98', '155.92', '170.19'],
-    'GOOGL': ['1209.24', '1209.66', '1299.54', '1300.19', '1327.26', '1299.12', '1300.00', '11301.91', '1308.67', '1350.11', '1400.01'],
-}
-
-tic_count = 0
-
-def update_users():
-    print("** update_users **")
-    t = Timer(10, update_users)
-    t.start()
-    global users
-    global tic_count
-
-    if tic_count < len(prices['MSFT'])-1: 
-        tic_count = tic_count + 1
-    else:
-        tic_count = 0
-    
-    for user in users:
-        symbol = user['symbol']
-        client.send_message(user['type'], user['to'], user['from'], prices[symbol][tic_count])
 
 update_users()
 
-def proc_inbound_msg(data):
-
-    global nexmo
-    global users
-
-    user = {}
-
-    channel_type = data['from']['type']
-    msg_text = data['message']['content']['text']
-
-    if channel_type == "messenger":
-        user['type'] = "messenger"
-        user['from'] = data['from']['id']
-        user['to'] = data['to']['id']
-
-    if 'MSFT' in msg_text:
-        user['symbol'] = "MSFT"
-        msg = "Registered for MSFT data."
-        users.append(user)
-    elif 'GOOGL' in msg_text:
-        user['symbol'] = "GOOGL"
-        msg = "Registered for GOOGL data."
-        users.append(user)
-    else:
-        msg = "Send us a message with MSFT or GOOGL in it for real-time data."
-
-    client.send_message(user['type'], user['to'], user['from'], msg)
-    return
-
 @app.route('/webhooks/inbound', methods=['POST'])
 def inbound_message():
-    global user
     print ("** inbound_message **")
     proc_inbound_msg(request.get_json())
     return ("inbound_message", 200)
@@ -87,7 +21,7 @@ def message_status():
 
 @app.route('/webhooks/inbound-sms', methods=['POST'])
 def inbound_sms():
-    print ("Inbound SMS:")
+    print ("** inbound_sms **")
     values = request.values
     for k, v in values.items():
         print(k, v)
@@ -97,7 +31,7 @@ def inbound_sms():
 
 @app.route('/webhooks/delivery-receipt', methods=['POST'])
 def delivery_receipt():
-    print ("Delivery receipt:")
+    print ("** delivery_receipt **")
     data = request.get_json()
     pprint(data)
     return ("delivery_receipt", 200)
---
+++ app_funcs.py
@@ -0,0 +1,72 @@
+import sys
+from threading import Timer
+from Client.Client import Client
+
+# Simulation of our real-time data feed
+# Arrays should be of same length
+prices = {
+    'MSFT': ['120.24', '119.36', '118.34', '119.78', '120.23', '122.98', '140.21', '141.09', '144.98', '155.92', '170.19'],
+    'GOOGL': ['1209.24', '1209.66', '1299.54', '1300.19', '1327.26', '1299.12', '1300.00', '11301.91', '1308.67', '1350.11', '1400.01'],
+}
+
+users = [] # array of registered users
+nexmo = {}
+nexmo['number'] = sys.argv[2]
+tic_count = 0
+
+NEXMO_APP_ID = sys.argv[1]
+print("Nexmo App ID: %s" % NEXMO_APP_ID)
+client = Client(NEXMO_APP_ID, "private.key")
+
+def update_users():
+    print("** update_users **")
+    t = Timer(10, update_users)
+    t.start()
+    global users
+    global tic_count
+
+    if tic_count < len(prices['MSFT'])-1: 
+        tic_count = tic_count + 1
+    else:
+        tic_count = 0
+    
+    for user in users:
+        symbol = user['symbol']
+        client.send_message(user['type'], user['to'], user['from'], prices[symbol][tic_count])
+    return
+
+def proc_inbound_msg(data):
+
+    global nexmo
+    global users
+
+    user = {}
+
+    channel_type = data['from']['type']
+    msg_text = data['message']['content']['text']
+
+    if channel_type == "messenger":
+        user['type'] = channel_type
+        user['from'] = data['from']['id']
+        user['to'] = data['to']['id']
+    elif channel_type == "whatsapp":
+        user['type'] = channel_type
+        user['from'] = data['from']['number']
+        user['to'] = data['to']['number']
+    else:
+        # Viber does not support inbound messages
+        print("ERROR: unrecognized channel type")
+        
+    if 'MSFT' in msg_text:
+        user['symbol'] = "MSFT"
+        msg = "Registered for MSFT data."
+        users.append(user)
+    elif 'GOOGL' in msg_text:
+        user['symbol'] = "GOOGL"
+        msg = "Registered for GOOGL data."
+        users.append(user)
+    else:
+        msg = "Send us a message with MSFT or GOOGL in it for real-time data."
+
+    client.send_message(user['type'], user['to'], user['from'], msg)
+    return
